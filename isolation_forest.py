64890811064090a0b1b096e86253f5ae51adf665
Alfred-Folashade <129620082+Alfred-Folashade@users.noreply.github.com>
2026-03-25 20:19:47 +0000

    Plotted data but need to add outliers to plot

--- isolation_forest.py
+++ isolation_forest.py
@@ -1,8 +1,13 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+
 
 data = np.random.randint(90, 110, size=1000)
-outliers = np.random.randint(140, 170 , size =25)
+outliers = np.random.randint(140, 170 , size =24)
+plt.scatter(data[0:499], data[500:999], )
+
+plt.show()
 data = np.append(data, outliers)
 outliers = np.random.randint(40, 70, size=25)
 glucose_readings = pd.DataFrame(data, columns = ['glucose_levels'])
