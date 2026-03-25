29260b2b600dd40388fd260e449e20a10990c212
Alfred-Folashade <129620082+Alfred-Folashade@users.noreply.github.com>
2026-03-25 19:44:11 +0000

    Add outliers to data

--- isolation_forest.py
+++ isolation_forest.py
@@ -2,5 +2,9 @@ import numpy as np
 import pandas as pd
 
 data = np.random.randint(90, 110, size=1000)
+outliers = np.random.randint(140, 170 , size =25)
+data = np.append(data, outliers)
+outliers = np.random.randint(40, 70, size=25)
 glucose_readings = pd.DataFrame(data, columns = ['glucose_levels'])
+
 print(glucose_readings)
