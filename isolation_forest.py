c198728fcfff420b56675e45ed636ac9b1e01ba3
Alfred-Folashade <129620082+Alfred-Folashade@users.noreply.github.com>
2026-04-11 21:33:20 +0100

    Update scatterplot to include outliers

--- isolation_forest.py
+++ isolation_forest.py
@@ -3,12 +3,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-data = np.random.randint(90, 110, size=1000)
+data = np.random.randint(90, 110, size=100)
 outliers = np.random.randint(140, 170 , size =24)
-plt.scatter(data[0:499], data[500:999], )
 
-plt.show()
 data = np.append(data, outliers)
+plt.scatter(data[0:62], data[62:125], )
+plt.show()
 outliers = np.random.randint(40, 70, size=25)
 glucose_readings = pd.DataFrame(data, columns = ['glucose_levels'])
 
